e161fa1509b4c8f57a49249e3eb54d07e38d827f
mpanczak <panczakmateusz@gmail.com>
2023-05-06 14:41:28 +0200

    day3, xml dependencies ex1

---
+++ src/main/java/pl/coderslab/app/SpringDiApplication.java
@@ -0,0 +1,22 @@
+package pl.coderslab.app;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pl.coderslab.beans.HelloWorld;
+
+public class SpringDiApplication {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        HelloWorld hi = (HelloWorld) context.getBean("hello"); // sposob 1
+//        HelloWorld hi1 = (HelloWorld) context.getBean("pl.coderslab.beans.HelloWorld"); // tylko jesli nie ma id w bean
+        HelloWorld hi2 = context.getBean("hello", HelloWorld.class); // sposob 3
+        HelloWorld hi3 = context.getBean(HelloWorld.class); // sposob 4
+
+        hi.printMessage();
+//        hi1.printMessage();
+        hi2.printMessage();
+        hi3.printMessage();
+
+        context.close();
+    }
+}
